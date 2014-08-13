9cea38c0f6ad33a55539489666e63b350b2691f9
Maxim <bhd.m@ya.ru>
2014-08-13 22:27:35 +0400

    add user

---
+++ src/Wzc/MainBundle/Entity/User.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace Wzc\MainBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Role\RoleInterface;
+
+/**
+ * Operator
+ *
+ * @ORM\Table()
+ * @ORM\Entity()
+ */
+class User extends BaseEntity implements UserInterface
+{
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    protected $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $password;
+
+    /**
+     * @ORM\Column(type="string", length=150)
+     */
+    protected $salt;
+
+    /**
+     * @ORM\Column(type="string", length=150)
+     */
+    protected $roles;
+
+    public function __construct(){
+        $this->roles    = 'ROLE_USER';
+    }
+
+    public function equals(UserInterface $user)
+    {
+        return md5($this->getUsername()) == md5($user->getUsername());
+    }
+
+    public function __toString()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRoles()
+    {
+        return explode(';', $this->roles);
+    }
+
+
+
+    /**
+     * @param mixed $roles
+     */
+    public function setRoles($roles)
+    {
+        if (is_array($roles)) {
+            $roles = implode($roles, ';');
+        }
+
+        $this->roles = $roles;
+    }
+
+    public function removeRole($role)
+    {
+        $roles = explode(';', $this->roles);
+        $key   = array_search($role, $roles);
+
+        if ($key !== false) {
+            unset($roles[$key]);
+            $this->roles = implode($roles, ';');
+        }
+    }
+
+    public function checkRole($role)
+    {
+        $roles = explode(';', $this->roles);
+
+        return in_array($role, $roles);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * Сброс прав пользователя.
+     */
+    public function eraseCredentials()
+    {
+        return true;
+    }
+
+    public function isEqualTo(UserInterface $user)
+    {
+        return md5($this->getUsername()) == md5($user->getUsername());
+    }
+
+    /**
+     * Сериализуем только id, потому что UserProvider сам перезагружает остальные свойства пользователя по его id
+     *
+     * @see \Serializable::serialize()
+     */
+    public function serialize()
+    {
+        return serialize(array(
+            $this->id
+        ));
+    }
+
+    /**
+     * @see \Serializable::unserialize()
+     */
+    public function unserialize($serialized)
+    {
+        list (
+            $this->id
+            ) = unserialize($serialized);
+    }
+    /**
+     * @return mixed
+     */
+    public function getSalt()
+    {
+        return $this->salt;
+    }
+
+    /**
+     * @param mixed $salt
+     */
+    public function setSalt($salt)
+    {
+        $this->salt = $salt;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+
+}

c5d96d2f554cba0f6c277a6f34c0010bf4320c01
Maxim <bhd.m@ya.ru>
2014-08-15 01:29:40 +0400

    update register

--- src/Wzc/MainBundle/Controller/DefaultController.php
+++ src/Wzc/MainBundle/Controller/DefaultController.php
@@ -5,7 +5,10 @@ namespace Wzc\MainBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\MessageDigestPasswordEncoder;
 use Wzc\MainBundle\Entity\Page;
+use Wzc\MainBundle\Entity\User;
 use Wzc\MainBundle\Entity\Slidebar;
 
 class DefaultController extends Controller
@@ -33,5 +36,30 @@ class DefaultController extends Controller
     {
         $page = $this->getDoctrine()->getRepository('WzcMainBundle:Page')->findOneByUrl($url);
         return array('page' => $page);
+    }
+
+    /**
+     * @Route("/register" ,name="register")
+     */
+    public function registerAction(Request $request){
+        $manager = $this->getDoctrine()->getManager();
+        if ($request->getMethod() == 'POST'){
+            $user = new User();
+            $user->setUsername($request->request->get('username'));
+            $user->setSalt(md5(time()));
+
+            // шифрует и устанавливает пароль для пользователя,
+            // эти настройки совпадают с конфигурационными файлами
+            $encoder = new MessageDigestPasswordEncoder('sha512', true, 10);
+            $password = $encoder->encodePassword($request->request->get('password'), $user->getSalt());
+            $user->setPassword($password);
+
+            $user->setRoles('ROLE_USER');
+
+            $manager->persist($user);
+            $manager->flush($user);
+
+        }
+        return $this->redirect($request->headers->get('referer'));
     }
 }

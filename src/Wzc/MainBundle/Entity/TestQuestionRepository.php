5bea9abebfd11333ae06dc1310c75294a967d32c
Maxim <bhd.m@ya.ru>
2014-09-02 00:53:28 +0400

    update testing

--- src/Wzc/MainBundle/Entity/TestQuestionRepository.php
+++ src/Wzc/MainBundle/Entity/TestQuestionRepository.php
@@ -7,14 +7,16 @@ use Doctrine\ORM\EntityRepository;
 class TestQuestionRepository extends EntityRepository
 {
     public function findNext($qId){
-        $result= $this
-            ->createQueryBuilder('q')
-            ->from('WzcMainBundle:testQuestion','q')
-            ->where(' q.id > :qId% ')
-            ->orderBy('q.id ASC')
+        $result= $this ->createQueryBuilder('q');
+        $result = $result
+            ->select('q')
+//            ->from('WzcMainBundle:TestQuestion','q')
+            ->where(' q.id > :qId ')
+            ->orderBy('q.id','ASC')
             ->setParameter('qId', $qId)
+            ->setMaxResults(1)
             ->getQuery()
-            ->getFirstResult();
+            ->getOneOrNullResult();
         return $result;
     }
 

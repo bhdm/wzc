5e10809da594b2cbaa01e03e2fa01258198bc838
bhdm <bhd.m@ya.ru>
2014-09-15 11:13:21 +0400

    update postcard

--- src/Wzc/MainBundle/Controller/PostcardController.php
+++ src/Wzc/MainBundle/Controller/PostcardController.php
@@ -18,39 +18,50 @@ class PostcardController extends Controller
     public function indexAction(Request $request)
     {
 
-        $form = $this->createFormBuilder()
+
+        $error = false;
+        $formData = null;
+        if ($request->getMethod() == 'POST'){
+            $formData = $request->request->get('form');
+            $c = $request->getSession()->get('gcb_captcha');
+            if ( $c['phrase'] == $formData['captcha']){
+                $email =$formData['email'];
+                $img = $this->getDoctrine()->getRepository('WzcMainBundle:Postcard')->find($formData['img']);
+                $this->get('email.service')->send(
+                    $email,
+//                array('zakaz@zdravzona.ru'),
+                    array('WzcMainBundle:Email:postcard.html.twig', array(
+                        'img' => $img,
+                        'title' => $formData['title'],
+//                    'text' => $request->request->get('text')
+                    )),
+                    'Открытка с сайта WZC'
+                );
+            }else{
+                $error = 'Неправильно введена капча';
+            }
+        }
+        $form = $this->createFormBuilder($formData)
             ->add('img', 'hidden')
+            ->add('title', 'hidden')
             ->add('email', null, array('label' => 'E-mail: ', 'attr' => array('class' => 'styler')))
             ->add('captcha', 'captcha', array('label' => 'Картинка: ', 'attr' => array('class' => 'styler')))
             ->add('submit', 'submit', array('label' => 'Отправить', 'attr' => array('class' => 'styler')))
             ->getForm();
 
 
-
-        if ($request->getMethod() == 'POST'){
-
-            $email =$request->request->get('email');
-            $img = $this->getDoctrine()->getRepository('WzcMainBundle:Postcard')->find($request->request->get('imgid'));
-            $this->get('email.service')->send(
-                $email,
-//                array('zakaz@zdravzona.ru'),
-                array('WzcMainBundle:Email:postcard.html.twig', array(
-                    'img' => $img,
-                    'title' => $request->request->get('title'),
-                    'text' => $request->request->get('text')
-                )),
-                'Открытка с сайта WZC'
-            );
-
-        }
-
         $postcards = $this->getDoctrine()->getRepository('WzcMainBundle:Postcard')->findByEnabled(1);
         $page = $this->getDoctrine()->getRepository('WzcMainBundle:Page')->findOneByUrl('postcard');
         $page2 = $this->getDoctrine()->getRepository('WzcMainBundle:Page')->findOneByUrl('postcard2');
 
-
-
-        return array('postcards' => $postcards, 'page' => $page, 'page2' => $page2, 'form' => $form->createView());
+        return array(
+            'postcards' => $postcards,
+            'page' => $page,
+            'page2' => $page2,
+            'form' => $form->createView(),
+            'error' => $error,
+            'formData' => $formData
+        );
     }
 
 

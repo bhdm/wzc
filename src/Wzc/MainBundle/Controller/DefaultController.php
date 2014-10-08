05fd82c718f38f304f02095cf3bf0b6457b71b3a
bhdm <bhd.m@ya.ru>
2014-10-08 10:55:26 +0400

    update nees in main

--- src/Wzc/MainBundle/Controller/DefaultController.php
+++ src/Wzc/MainBundle/Controller/DefaultController.php
@@ -25,6 +25,9 @@ class DefaultController extends Controller
     {
         $message = $this->getDoctrine()->getRepository('WzcMainBundle:Page')->findOneByUrl('message');
         $banners = $this->getDoctrine()->getRepository('WzcMainBundle:Slidebar')->findByEnabled(1);
+        $news = $this->getDoctrine()->getRepository('WzcMainBundle:Publication')->findByEnabled(1);
+        krsort($news);
+        $news = array_slice($news,0,3);
 
         //$session = $request->getSession();
         //$session->set('notice', 'Поздравляем, Вы зарегистрировались.');
@@ -42,7 +45,8 @@ class DefaultController extends Controller
         return array(
             'banners' => $banners,
             'message' => $message,
-            'notice' => $notice
+            'notice' => $notice,
+            'news' => $news
         );
     }
 
